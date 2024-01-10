6441b3cf6a21e0110f2fa5eb308e4b56aca034a9
Sunga Thawethe <sungabt@gmail.com>
2024-01-10 16:35:55 +0200

    Create bigday comp

---
+++ src/components/BigDay.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const BigDay = () => {
+  return (
+    <div id="bigday">
+      <h1>The Big Day</h1>
+      <p>
+        We can't wait to celebrate with everyone! Below are the details for our Marriage ceremony & reception! Come ready to dance and celebrate God's goodness!
+        <br />
+        <br />
+        <h4>Marriage Blessing:</h4>
+        First Love Church Ndola, Opposite Zesco Training Centre & Uno Filling Station.
+        <br />
+        <h4>Time:</h4>
+        10:00 - 12:00
+        <br />
+        <br />
+        <h4>Reception:</h4>
+        Izu Hotel, Ground Floor Hall
+        <br />
+        <h4>Time:</h4>
+        14:00 - 17:00
+        <h2>S & K</h2>
+        
+      </p>
+    </div>
+  )
+}
+
+export default BigDay
