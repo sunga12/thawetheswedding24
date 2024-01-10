b16e360bc8da48a91ac3cc0a6adf4433afc370d5
Sunga Thawethe <sungabt@gmail.com>
2024-01-10 16:38:19 +0200

    Add gallery.js

---
+++ src/gallery.js
@@ -0,0 +1,30 @@
+import SK1 from './assets/images/SK/SK1.jpg';
+import SKbc from './assets/images/SK/SKbc.jpg';
+import SKbc1 from './assets/images/SK/SKbc1.jpg';
+import SKflnd from './assets/images/SK/SKflnd.jpg';
+import SKfldn1 from './assets/images/SK/SKflnd1.jpg';
+
+const gallery = [
+  {
+    id: 1,
+    file: SK1,
+  }, 
+  {
+    id: 2,
+    file: SKbc
+  },
+  {
+    id: 3,
+    file: SKbc1  
+  },
+  {
+    id: 4,
+    file: SKflnd
+  }, 
+  {
+    id: 5,
+    file: SKfldn1
+  }, 
+];
+
+export default gallery;
