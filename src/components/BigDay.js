ce9564c30a65a75a24e28934fb88a0cf8347ee5e
Sunga Thawethe <sungabt@gmail.com>
2024-01-15 21:23:35 +0200

    Bigday descktop version

--- src/components/BigDay.js
+++ src/components/BigDay.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion';
+import skbigday from '../assets/images/SK/SKflnd.jpg'
 
 const dayVariants = {
   hidden: {
@@ -18,25 +19,29 @@ const BigDay = () => {
   return (
     <div id="bigday">
       <motion.h1 variants={dayVariants} initial="hidden" whileInView="visible" viewport={{ once: true }}>The Big Day</motion.h1>
-      <motion.p variants={dayVariants} initial="hidden" whileInView="visible" viewport={{ once: true }}>
-        We can't wait to celebrate with everyone! Below are the details for our Marriage ceremony & reception! Come ready to dance and celebrate God's goodness!
-        <br />
-        <br />
-        <h4>Marriage Blessing:</h4>
-        First Love Church Ndola, Opposite Zesco Training Centre & Uno Filling Station.
-        <br />
-        <h4>Time:</h4>
-        <span className="bold-text">10:00</span>
-        <br />
-        <br />
-        <h4>Reception:</h4>
-        Izu Hotel, Ground Floor Hall
-        <br />
-        <h4>Time:</h4>
-        <span className="bold-text">17:00</span>
-        <h2>S & K</h2>
-        
-      </motion.p>
+      <div className="bigday-deets">
+        <motion.p variants={dayVariants} initial="hidden" whileInView="visible" viewport={{ once: true }}>
+          We can't wait to celebrate with everyone! Below are the details for our Marriage Ceremony & Reception!
+          <br />
+          Come ready to dance and celebrate God's goodness!
+          <br />
+          <h4>Marriage Blessing:</h4>
+          First Love Church Ndola, Opposite Zesco Training Centre & Uno Filling Station.
+          <br />
+          <h4>Time:</h4>
+          <span className="bold-text">10:00</span>
+          <br />
+          <h4>Reception:</h4>
+          Izu Hotel, Ground Floor Hall
+          <br />
+          <h4>Time:</h4>
+          <span className="bold-text">17:00</span>          
+        </motion.p>
+        <div>
+          <img className="skbigday" src={skbigday} alt="sk" />
+        </div>
+      </div>
+      <h2>S & K</h2>
     </div>
   )
 }
